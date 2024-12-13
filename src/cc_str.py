e4b5bf6334c6eaa671d98d1c8bd0024032acce2b
prodhanm <mustafizur.prodhan@gmail.com>
2024-12-12 20:08:50 -0500

    added an input function to allow for dynamic programming

--- src/cc_str.py
+++ src/cc_str.py
@@ -2,6 +2,8 @@ class CreditCardStr:
     def __init__(self, card_number):
         self.card_number = card_number
 
+    cc_inp = input("Enter your credit card number: ")
+
     @staticmethod
     def cc_format_string(card_number):
         return card_number.replace("-", "")
