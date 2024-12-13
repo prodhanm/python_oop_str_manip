87d7b32dafe9f255dab9fcb00daf41b9b41f6ace
prodhanm <mustafizur.prodhan@gmail.com>
2024-12-12 19:19:06 -0500

    the program works succesfully

---
+++ src/cc_str.py
@@ -0,0 +1,11 @@
+class CreditCardStr:
+    def __init__(self, card_number):
+        self.card_number = card_number
+
+    @staticmethod
+    def cc_format_string(card_number):
+        return card_number.replace("-", "")
+    
+formatted_cc = CreditCardStr.cc_format_string("1234-5678-1234-5678")
+print(formatted_cc)
+    
