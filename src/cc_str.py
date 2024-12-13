8e8f532f19ee85e353ca56221a3f4242ddd312f1
prodhanm <mustafizur.prodhan@gmail.com>
2024-12-12 20:21:07 -0500

    rearranged he input code to ensure that the code actually works and it works

--- src/cc_str.py
+++ src/cc_str.py
@@ -2,12 +2,12 @@ class CreditCardStr:
     def __init__(self, card_number):
         self.card_number = card_number
 
-    cc_inp = input("Enter your credit card number: ")
 
     @staticmethod
     def cc_format_string(card_number):
         return card_number.replace("-", "")
     
-formatted_cc = CreditCardStr.cc_format_string("1234-5678-1234-5678")
+card_number = input("Enter your credit card number: ")
+formatted_cc = CreditCardStr.cc_format_string(card_number)
 print(formatted_cc)
     
